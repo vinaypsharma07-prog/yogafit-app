7c5a9f2cc2936bea7f62362d1da589bafd001834
vinaypsharma07-prog <vinaypsharma07@gmail.com>
2026-01-22 19:41:08 +0530

    Create yoga-data.js

---
+++ yoga-data.js
@@ -0,0 +1,22 @@
+const yogaData = [
+  {
+    name: "Surya Namaskar",
+    duration: "10 minutes",
+    benefits: "Improves flexibility and boosts energy"
+  },
+  {
+    name: "Bhujangasana",
+    duration: "5 minutes",
+    benefits: "Strengthens spine and reduces back pain"
+  },
+  {
+    name: "Vrikshasana",
+    duration: "5 minutes",
+    benefits: "Improves balance and concentration"
+  },
+  {
+    name: "Padmasana",
+    duration: "10 minutes",
+    benefits: "Improves posture and calmness"
+  }
+];
