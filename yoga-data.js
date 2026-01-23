a417a0c639437fc28440c854746faab51d74aa63
vinaypsharma07-prog <vinaypsharma07@gmail.com>
2026-01-23 18:25:50 +0530

    Update yoga-data.js

--- yoga-data.js
+++ yoga-data.js
@@ -1,6 +1,14 @@
-let yogaData = JSON.parse(localStorage.getItem("yogaData")) || [
-  "Surya Namaskar",
-  "Bhujangasana",
-  "Vrikshasana",
-  "Padmasana"
-];
+const plans = {
+  "Morning_Weight Loss": [
+    { name:"Surya Namaskar", img:"https://i.imgur.com/0KQ9vYc.png", benefit:"Fat burn" },
+    { name:"Kapalbhati", img:"https://i.imgur.com/3l1k0Qn.png", benefit:"Belly fat" }
+  ],
+  "Morning_Fitness": [
+    { name:"Vrikshasana", img:"https://i.imgur.com/cQq1ZzG.png", benefit:"Balance" },
+    { name:"Bhujangasana", img:"https://i.imgur.com/1E6FZ3T.png", benefit:"Spine" }
+  ],
+  "Evening_Stress Relief": [
+    { name:"Padmasana", img:"https://i.imgur.com/Mo4rV2M.png", benefit:"Calm" },
+    { name:"Pranayama", img:"https://i.imgur.com/6vY9mQp.png", benefit:"Relax" }
+  ]
+};
